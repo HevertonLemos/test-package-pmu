d9de7d1275a0c267135e144ce02e8b863e7363b3
PIRES, ADRIANO OLIVEIRA (Adriano) <adriano.pires@ge.com>
2017-11-10 18:39:15 -0200

    Added option to start_lib to be used from other path

--- start_lib.m
+++ start_lib.m
@@ -1,5 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
+% Usage: start_lib(lib_path)
+%
 % This function start the modules necessary to correct performance the lib_pmu.
 % Too charge the all modules (folders).
 %
@@ -8,7 +10,7 @@
 %
 %  Heverton de Lemos 24/10/2017
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function start_lib()
+function start_lib(lib_path)
 
   warning off;
   #start package sockets
@@ -20,15 +22,19 @@ function start_lib()
   elseif( strcmp(flag{1},'Not loaded') )
     pkg load sockets;
   endif
+  
+  if (nargin < 1)
+    lib_path = pwd;
+  endif
 
   #start folders
-  addpath( strcat(pwd,'/support') );
-  addpath( strcat(pwd,'/communication') );
-  addpath( strcat(pwd,'/c37118.1') );
-  addpath( strcat(pwd,'/c37118.1/steady_state') );
-  addpath( strcat(pwd,'/c37118.1/dynamic_state') );
-  addpath( strcat(pwd,'/c37118.2') );
-  addpath( strcat(pwd,'/data') );
+  addpath( strcat(lib_path,'/support') );
+  addpath( strcat(lib_path,'/communication') );
+  addpath( strcat(lib_path,'/c37118.1') );
+  addpath( strcat(lib_path,'/c37118.1/steady_state') );
+  addpath( strcat(lib_path,'/c37118.1/dynamic_state') );
+  addpath( strcat(lib_path,'/c37118.2') );
+  addpath( strcat(lib_path,'/data') );
 
 
 endfunction
